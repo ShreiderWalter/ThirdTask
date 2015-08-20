04296131c7b6e0eb966b78a1a84f93a569dfb577
sbabenya <trippelwalter@gmail.com>
2015-08-20 16:58:02 +0300

    cross platform improved

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
+#ifdef __linux__
 #include "LinuxSystemResourceHelper.h"
+#define PLATFORM LinuxSystemResourceHelper
+#elif defined _WIN32 || defined _WIN64
+#include "WindowsSystemResourceHelper.h"
+#define PLATFORM WindowsSystemResourceHelper
+#endif
+
+#include "SystemResourceHelper.h"
 
 int main()
 {
-    LinuxSystemResourceHelper * helper = LinuxSystemResourceHelper::getInstance();
-    //std::cout << helper->getCPUUsage() << "%\n";
-    //std::cout << helper->getMemoryUsage() << "%\n";
+	SystemResourceHelper<PLATFORM> * helper = SystemResourceHelper<PLATFORM>::getInstance();
+	std::cout << helper->getCPUUsage() << "%\n";
+	//std::cout << helper->getMemoryUsage() << "%\n";
+	/*std::list<std::string> disks = helper->getDisks();
+	for(auto i : disks)
+	{
+		std::cout << i << "\n";
+	}*/
 
-    /*std::list<std::string> disks = helper->getDisks();
+	//std::cout << helper->getDiskUsage("c:") << "%\n";
 
-    for(auto i : disks)
-    {
-        std::cout << i << "\n";
-    }*/
+    delete helper;
+    helper = nullptr;
 
-    //std::cout << helper->getDiskUsage("/var");
-    return 0;
+	return 0;
 }
-
